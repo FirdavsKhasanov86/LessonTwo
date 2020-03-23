d6221d153ae519bbd5ebf060ba661fa3bb08fda5
user <user@Williams-iMac.local>
2020-03-23 11:53:04 +0500

    lessonTwo

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HomeWork - LessonTwo
+
+            //1.
+            //double a = 16.80,b =12.40,c,d;
+            //c = a * b;
+            //d = Math.Sqrt(c);
+            //Console.WriteLine(d);
+
+            //---------------------------//
+
+            //2.
+            // double a = 1.4, b = -5.5, c = 0.6, ac, bc;
+            // ac = a - c;
+            // bc = c - (b);
+            // Console.WriteLine("AC+BC = " + (ac + bc));
+
+            //-------------------------------------------//
+
+            //3.
+            // double x1 = -6.2, y1 = 5.2, x2 = 2.1, y2 = 9.8, a,b,c,d,e,f;
+            // a = x2 - (x1);
+            // b = Math.Pow(a,2);
+            
+            // c = y2 - y1;
+            // d = Math.Pow(c,2);
+   
+            // e = b + d;
+            // f = Math.Sqrt(e);
+
+            // Console.WriteLine(f);
+
+           //------------------------------------------//
+
+           //4.
+           //int a = 41,b,c;
+           //b = a % 10;
+           //c = a / 10;
+           
+           //Console.Write(b);
+           //Console.Write(c); 
+
+          //-------------------------------------------//
+
+          //5.
+          //   int a = 10985,b;
+          //   b = a / 60;
+          //   Console.Write(b);
+
+          //-------------------------------------------//
+
+          //6.
+          //    int K=202,N =1;
+          //    int day = Math.Abs((N + K - 1) % 7);
+
+          //    Console.WriteLine(day);
+
+        }
+    }
+}
